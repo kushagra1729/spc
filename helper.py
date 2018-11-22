8fd2740d2dcf4881cdd899fe6b8f5c7817a6ab32
kritin2 <kritingarg0209@gmail.com>
2018-11-23 01:40:31 +0530

    final_spc

---
+++ helper.py
@@ -0,0 +1,11 @@
+import os
+cwd = os.getcwd()
+print(cwd)
+# dir = os.path.dirname(__file__)
+# path = input()
+# if os.path.isabs(path):
+#     print ("input path is absolute")
+# else:
+#     path = os.path.join(dir, path)
+#     print(path)
+#     print ("absolute path is %s")
